5786baf321b0371feea079ea60ca6c310bb83f5f
ammaar345 <fakirammaar8@gmail.com>
2020-08-20 20:33:41 +0200

    added a new route

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars');
 const bodyParser = require("body-parser")
 // 
 const app = express();
+const Greet=require("./greet");
+const greet=Greet();
 // const flash = require('express-flash');
 // const session = require('express-session');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -14,14 +16,30 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
- //
+ 
+//console.log(greet.greeter())
+
+
+
+
  app.get('/', function (req, res) {
   res.render("index");
 })
 
 
 
-  const PORT = process.env.PORT || 3011;
+app.post("/action",function(req,res){
+  res.redirect("/")
+  greet.greetings(req.body.actionType )
+  console.log(greet.names())
+  console.log(greet.greetings(req.body.languageType ))
+})
+// console.log(greet.names())
+
+
+
+
+  const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
 
     console.log("App started at port:", PORT);
