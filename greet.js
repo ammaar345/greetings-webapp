cc87ccf313ab0520cc83c25c87c47b51b04c61d5
Ammaar <fakirammaar8@gmail.com>
2020-09-09 11:44:59 +0200

    fixed error of adding ''' into db

--- greet.js
+++ greet.js
@@ -1,14 +1,15 @@
 module.exports = function Greet() {
     const pg = require("pg");
     const Pool = pg.Pool;
-    const connectionString = process.env.DATABASE_URL || 'postgresql://sneakygoblin:codex123@localhost:5432/greetings-webapp';
+    const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/greetings-webapp';
     const pool = new Pool({
         connectionString
     });
     var nameMap = {};
     async function addEntry(param) {
-        const INSERT_QUERY = ' insert into users (name,count) values ($1,1)';
-        await pool.query(INSERT_QUERY, [param.name]);
+       if(param.name!==''){
+        const INSERT_QUERY = ' insert into users (name,greeted_count) values ($1,1)';
+        await pool.query(INSERT_QUERY, [param.name]);}
     }
 
     //    async function names(userN) {
@@ -21,6 +22,23 @@ module.exports = function Greet() {
     //         }
     //         nameMap[userName]++
     //     }
+//async function greetings (name, language) {
+    
+  
+
+    async  function  countGreeted (parameter){
+        const SELECT_QUERY='Select name from users where name=$1'
+const UPDATE_QUERY='UPDATE users set greeted_count=greeted_count+1  where name=$1 ';
+
+if (SELECT_QUERY){
+    await pool.query(UPDATE_QUERY)
+}
+else {
+    addEntry(parameter)
+}
+
+    }
+
     async function singleNameCount(nameParam) {
 
         return await nameMap[nameParam.charAt(0).toUpperCase() + await nameParam.toLowerCase().slice(1)]
@@ -77,6 +95,7 @@ module.exports = function Greet() {
         getNames,
         flshMsg,
         singleNameCount,
-        addEntry
+        addEntry,
+        countGreeted
     }
 }
