a8909e352bf5b20e42df033dfcfc2a9bf5736daf
Ammaar <fakirammaar8@gmail.com>
2020-09-15 18:23:11 +0200

    removed comments

--- test/greet.test.js
+++ test/greet.test.js
@@ -16,20 +16,20 @@ describe("Tests greeting the user in the language selected.", function () {
 
 	describe("Tests if counting function works correctly.", function () {
 		it("Should count 3 names in the database.", async function () {
-			let greet = Greet()
+			let greet = Greet(pool)
 			await pool.query(INSERT_QUERY, ['Ammaar'])
 			await pool.query(INSERT_QUERY, ['Lucy'])
 			await pool.query(INSERT_QUERY, ['John'])
 			assert.equal(await greet.nameCounter(), 3)
 		})
 		it("Should count 2 names in the database.", async function () {
-			let greet = Greet()
+			let greet = Greet(pool)
 			await pool.query(INSERT_QUERY, ['Ammaar'])
 			await pool.query(INSERT_QUERY, ['John'])
 			assert.equal(await greet.nameCounter(), 2)
 		})
 		it("Should count 6 names in the database.", async function () {
-			let greet = Greet()
+			let greet = Greet(pool)
 			await pool.query(INSERT_QUERY, ['Henderson'])
 			await pool.query(INSERT_QUERY, ['Erin'])
 			await pool.query(INSERT_QUERY, ['Thabo'])
@@ -39,13 +39,13 @@ describe("Tests greeting the user in the language selected.", function () {
 			assert.equal(await greet.nameCounter(), 6)
 
 			it("Should count 1 name in the database , since the name Ammaar is already in the database.", async function () {
-				let greet = Greet()
+				let greet = Greet(pool)
 				await pool.query(INSERT_QUERY, ['Ammaar'])
 				await pool.query(INSERT_QUERY, ['ammaar'])
 				assert.equal(await greet.nameCounter(), 1)
 			})
 			it("Should count 2 names in the database since the name Lucy is already in the database.", async function () {
-				let greet = Greet()
+				let greet = Greet(pool)
 				await pool.query(INSERT_QUERY, ['Lucy'])
 				await pool.query(INSERT_QUERY, ['lucy'])
 				await pool.query(INSERT_QUERY, ['John'])
@@ -56,19 +56,19 @@ describe("Tests greeting the user in the language selected.", function () {
 
 
 			it("Should greet the username Ammaar in English.", async function () {
-				let greet = Greet();
+				let greet = Greet(pool);
 				var userName = "Ammaar"
 				var language = "English";
 				assert.equal(await greet.greetUser(userName, language), "Hello, Ammaar");
 			});
 			it("Should greet the username Thomas in Afrikaans.", async function () {
-				let greet = Greet();
+				let greet = Greet(pool);
 				var userName = "Thomas"
 				var language = "Afrikaans";
 				assert.equal(await greet.greetUser(userName, language), "Halo, Thomas");
 			});
 			it("Should greet the username John in Xhosa.", async function () {
-				let greet = Greet();
+				let greet = Greet(pool);
 				var userName = "Unalo";
 				var language = "Xhosa";
 				assert.equal(await greet.greetUser(userName, language), "Mholo, Unalo");
@@ -79,12 +79,12 @@ describe("Tests greeting the user in the language selected.", function () {
 	})
 	describe("Shows the names being stored in the object.", function () {
 		it(" Should return 1 name in the list object that was added to the database.", async function () {
-			let greet = Greet()
+			let greet = Greet(pool)
 			await pool.query(INSERT_QUERY, ['Yolanda'])
 			assert.deepEqual(await greet.getNames(), [{ name: "Yolanda" }])
 		})
 		it("Should return 2 names in the list object that were added to the database.", async function () {
-			let greet = Greet()
+			let greet = Greet(pool)
 			await pool.query(INSERT_QUERY, ['Tom'])
 			await pool.query(INSERT_QUERY, ['Jerry'])
 			assert.deepEqual(await greet.getNames(), [{
@@ -93,13 +93,12 @@ describe("Tests greeting the user in the language selected.", function () {
 			{ name: "Jerry" }])
 		})
 		it("Should return 4 names in the list object that were added to the database.", async function () {
-			let greet = Greet()
+			let greet = Greet(pool)
 			await pool.query(INSERT_QUERY, ['Joe'])
 			await pool.query(INSERT_QUERY, ['Lewis'])
 			await pool.query(INSERT_QUERY, ['Themba'])
 			await pool.query(INSERT_QUERY, ['Johnathan'])
 
-			// greeting.names("Nwabile")
 			assert.deepEqual(await greet.getNames(), [{ name: "Joe" },
 			{ name: "Lewis" },
 			{ name: "Themba" },
@@ -111,20 +110,20 @@ describe("Tests greeting the user in the language selected.", function () {
 
 
 		it("Should add Joe to the database and then increase the counter if the name Joe exists after being added.", async function () {
-			let greet = Greet();
+			let greet = Greet(pool);
 			var userName = "Joe"
 			await greet.countGreeted(userName)
 			await greet.countGreeted(userName)
 			assert.equal(await greet.singleNameCount(userName), 2);
 		});
 		it("Should add Thomas to the database since Thomas is not in the database already.", async function () {
-			let greet = Greet();
+			let greet = Greet(pool);
 			var userName = "Thomas"
 			await greet.countGreeted(userName)
 			assert.equal(await greet.singleNameCount(userName), 1);
 		});
 		it("Should add the name Unalo to the database if its not there , but after unalo is added to the database the counter should increase 3 more times.", async function () {
-			let greet = Greet();
+			let greet = Greet(pool);
 			var userName = "Unalo";
 			await greet.countGreeted(userName);
 			await greet.countGreeted(userName);
@@ -139,7 +138,7 @@ describe("Tests greeting the user in the language selected.", function () {
 
 
 		it("Should clear 3 names from the database.", async function () {
-			let greet = Greet();
+			let greet = Greet(pool);
 			await pool.query(INSERT_QUERY, ['Joe'])
 			await pool.query(INSERT_QUERY, ['Lewis'])
 			await pool.query(INSERT_QUERY, ['Themba'])
@@ -147,14 +146,14 @@ describe("Tests greeting the user in the language selected.", function () {
 			assert.deepEqual(await greet.getNames(),[]);
 		});
 		it("Should clear 2 names from the database.", async function () {
-			let greet = Greet();
+			let greet = Greet(pool);
 			await pool.query(INSERT_QUERY, ['Tom'])
 			await pool.query(INSERT_QUERY, ['Jerry'])
 			await greet.clearDB()
 			assert.deepEqual(await greet.getNames(), []);
 		});
 		it("Should clear 1 name from the database.", async function () {
-			let greet = Greet();
+			let greet = Greet(pool);
 			await pool.query(INSERT_QUERY, ['Yolanda'])
 			await greet.clearDB()
 			assert.deepEqual(await greet.getNames(), []);
